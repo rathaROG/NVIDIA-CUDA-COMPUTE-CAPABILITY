33b405358f5477f8461a5dd7bf3d5cb1e2efca1a
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-24 21:28:13 +0100

    Update test_functions.py
    Add static result comparisions when possible

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -14,23 +14,29 @@ def test_get_arches_basic():
     assert isinstance(result, list) or isinstance(result, str)
     result = get_arches(cuda_ver='13.0', min_sm=75)
     assert isinstance(result, list)
+    assert result == ['75', '80', '86', '87', '88', '89', '90', '100', '103', '110', '120', '121']
 
-def test_get_arches_cons_jets():
+def test_get_arches_full():
     from nvidia_arch import get_arches
     result = get_arches(gpu_type='cons', cuda_ver='13.0', min_sm=75, return_mode='cc_string', add_ptx=True)
     assert isinstance(result, str)
+    assert result == '7.5;8.6;8.9;12.0;12.1+PTX'
     result = get_arches(gpu_type='jets', cuda_ver='12.8', min_sm=60, return_mode='cc_list', add_ptx=True)
     assert isinstance(result, list)
+    assert result == ['6.2', '7.2', '8.7', '10.1+PTX']
     result = get_arches(gpu_type='dcen', cuda_ver='12.9', min_sm=30, return_mode='cc_list', add_ptx=True)
     assert isinstance(result, list)
+    assert result == ['5.2', '6.0', '6.1', '7.0', '7.5', '8.0', '8.6', '8.9', '9.0', '10.0', '10.3', '12.0+PTX']
     result = get_arches(gpu_type='cons+jets', cuda_ver='13.2', min_sm=75, return_mode='sm_list', add_ptx=True)
     assert isinstance(result, list)
+    assert result == ['75', '86', '87', '89', '110', '120', '121+PTX']
 
 def test_get_arches_make_gencode_flags():
     from nvidia_arch import get_arches, make_gencode_flags
     arches = get_arches(gpu_type='jets', cuda_ver='13.0', min_sm=75)
     flags = make_gencode_flags(arches, add_ptx=True)
     assert isinstance(flags, list)
+    assert flags == ['-gencode=arch=compute_87,code=sm_87', '-gencode=arch=compute_110,code=[sm_110,compute_110]']
 
 def test_detect_ctk():
     from nvidia_arch import detect_ctk
@@ -77,4 +83,4 @@ if __name__ == "__main__":
     test_find_gpus()
     test_validate_arch_string_success()
     test_validate_cc_string_exception()
-    print("All basic workflow tests passed!")
+    print("All tests passed!")
